d1936c4a7cc5cfa5084ba352e184e058332eef65
Yuebook <mwm0022@126.com>
2026-05-17 20:22:57 +0800

    升级成多进程启动方式。

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,17 +4,23 @@ module.exports = {
       name: 'maccms-node',
       cwd: __dirname,
       script: 'app.js',
-      instances: 1,
-      exec_mode: 'fork',
+      instances: 'max',
+      exec_mode: 'cluster',
       autorestart: true,
       watch: false,
-      max_memory_restart: '512M',
+      max_memory_restart: '768M',
+      instance_var: 'NODE_APP_INSTANCE',
+      merge_logs: true,
       time: true,
       env: {
-        NODE_ENV: 'development'
+        NODE_ENV: 'development',
+        ENABLE_CRON: 'true',
+        CRON_PRIMARY_ONLY: 'true'
       },
       env_production: {
-        NODE_ENV: 'production'
+        NODE_ENV: 'production',
+        ENABLE_CRON: 'true',
+        CRON_PRIMARY_ONLY: 'true'
       }
     }
   ]
